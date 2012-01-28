c3520cfcdaf7c636dc699bf09fe190ab8812e4f2
Tatu Saloranta <tsaloranta@gmail.com>
2012-01-28 13:34:48 -0800

    ...

---
+++ src/main/java/com/fasterxml/jackson/jaxrs/json/annotation/JSONP.java
@@ -0,0 +1,34 @@
+package com.fasterxml.jackson.jaxrs.json.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/* Note: applicable to annotations to allow bundling (if support added
+ * to JAX-RS bundle itself), as well as methods to indicate that return
+ * type is to be wrapped.
+ * Other types are not allowed, since there is no current usage for those;
+ * input can't be wrapped (so no need for parameters); fields are not
+ * exposed through JAX-RS; and we do not allow 'default wrapping' for
+ * types.
+ *<p>
+ * Note on properties: if either {@link #prefix()} or {@link #suffix()}
+ * is non-empty, they are used as literal prefix and suffix to use.
+ * Otherwise {@link #value()} is used as the function name, followed
+ * by opening parenthesis, value, and closing parenthesis.
+ *<p>
+ * Example usage:
+ *<pre>
+ *</pre>
+ */
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@com.fasterxml.jackson.annotation.JacksonAnnotation
+public @interface JSONP
+{
+    public String value() default "";
+    
+    public String prefix() default "";
+    public String suffix() default "";
+}
